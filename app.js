c84628f134a4dbc7d151a3d28ddb3eb3fb5b529a
elminhuseynov <elmin.huseynov0821@gmail.com>
2024-11-11 19:10:06 +0400

    homework6

---
+++ app.js
@@ -0,0 +1,52 @@
+let a = 1200;
+let b = 20;
+
+
+function result(number) {
+    console.log(a + (b * number));
+}
+result(75)
+
+let c = +prompt("1-99 arasi reqem daxil edin");
+
+switch (c) {
+    case 1:
+        console.log("bir");
+        break;
+    case 2:
+        console.log("iki");
+        break;
+   
+    case 3:
+        console.log("uc");
+        break;
+   
+    case 4:
+        console.log("dort");
+        break;
+   
+    case 5:
+        console.log("bes");
+        break;
+   
+    case 6:
+        console.log("alti");
+        break;
+   
+    case 7:
+        console.log("yeddi");
+        break;
+   
+    case 8:
+        console.log("sekkiz");
+        break;
+   
+    case 9:
+        console.log("doqquz");
+        break;
+   
+    case 10:
+        console.log("on");
+        break;
+}
+
